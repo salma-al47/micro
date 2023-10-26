65c925e74fbbf04d95cf8f3ff0cd5eb920b4abb9
salma-al47 <salmaalouli21@gmail.com>
2023-10-26 19:39:43 +0200

    first commit

--- api/index.js
+++ api/index.js
@@ -34,6 +34,4 @@ app.get('/', (req, res) =>
     res.send(`Node and express server is running on port ${PORT}`)
 );
 
-app.listen(PORT, () =>
-    console.log(`your server is running on port ${PORT}`)
-);
+
